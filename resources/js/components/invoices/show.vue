3b3464d405f06d4e4ad8f594096208636f51fa26
Eunbi Yoon <yinyinbei0717@gmail.com>
2023-11-07 09:35:34 -0600

    werwer

--- resources/js/components/invoices/show.vue
+++ resources/js/components/invoices/show.vue
@@ -14,9 +14,9 @@
         getInvoice()
     })
     const getInvoice=async()=>{
-        let response=await axios.get(`/api/show_invoice/${props.id}`)
-        console.log('form', response.data.invoice)
-        form.value=response.data.invoice
+        let response=await axios.get(`/api/show_invoice/${props.id}`);
+        console.log('form', response.data.invoice);
+        form.value=response.data.invoice;
     }
 </script>
 <template>
